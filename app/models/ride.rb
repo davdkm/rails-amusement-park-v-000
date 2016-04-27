f98306b51cba700d22b40e57fd453d151c8dd6be
davdkm <davd.km@gmail.com>
2016-04-27 13:26:51 -0700

    user model tests passing

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,17 @@
 class Ride < ActiveRecord::Base
   # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if attraction.tickets > user.tickets && attraction.min_height > user.height
+      "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif self.attraction.tickets > self.user.tickets
+      "Sorry. You do not have enough tickets the #{attraction.name}."
+    elsif self.attraction.min_height > self.user.height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      user.update(tickets: (user.tickets -= attraction.tickets), nausea: (user.nausea += attraction.nausea_rating), happiness: (user.happiness += attraction.happiness_rating))
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if nausea > happiness
+      'sad'
+    elsif nausea < happiness
+      'happy'
+    end
+  end
 end
